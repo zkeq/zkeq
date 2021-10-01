275d50d04aca6d31ee3c5b38e2ef8777ff6b768c
zkeq <62864752+zkeq@users.noreply.github.com>
2021-10-01 08:44:40 +0800

    Update functions.js

--- shuju/functions.js
+++ shuju/functions.js
@@ -55,6 +55,6 @@ function timeElapse(date){
 	if (seconds < 10) {
 		seconds = "0" + seconds;
 	}
-	var result = "第 <span class=\"digit\">" + days + "</span> 天 <span class=\"digit\">" + hours + "</span> 小时 <span class=\"digit\">" + minutes + "</span> 分钟 <span class=\"digit\">" + seconds + "</span> 秒"; 
+	var result = "▧ <span class=\"digit\">" + days + "</span> ▧ <span class=\"digit\">" + hours + "</span> ▧▧ <span class=\"digit\">" + minutes + "</span> ▧▧ <span class=\"digit\">" + seconds + "</span> ▧"; 
 	$("#clock").html(result);
 }
