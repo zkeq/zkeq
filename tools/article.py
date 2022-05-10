40c6c7bd5454da69ba9fdea2fabecdcf5d772006
zkeq <admin@icodeq.com>
2022-05-11 01:52:49 +0800

    更新时间戳防止无git

--- tools/article.py
+++ tools/article.py
@@ -1,6 +1,7 @@
 # coding:utf-8
 import requests
 import os
+import time
 
 url = "https://fcircle.icodeq.com/post?num=10"
 
@@ -13,11 +14,18 @@ with open(os.path.join("article.svg"), "r", encoding="utf-8") as f:
     svg = f.read()
 
 
+# 获取当前时间
+def get_now():
+    now = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+    return now
+
+
 for i in range(len(article_list)):
     item = article_list[i]["title"]
     if len(item) > 22:
         item = item[:22] + ".."
     svg = svg.replace("{title-%s}" % (i + 1), item)
+    svg = svg.replace("{time}", get_now())
 
 
 # 写入 \profile\article.svg
