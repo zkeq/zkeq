2111566eab8115b68139252f31ce983429588e08
zkeq <admin@icodeq.com>
2022-12-17 11:53:58 +0800

    添加亮色文章

--- tools/article.py
+++ tools/article.py
@@ -9,27 +9,35 @@ data = requests.get(url).json()
 
 article_list = data["article_data"]
 
-# 读取 article.svg 文件
-with open(os.path.join("article.svg"), "r", encoding="utf-8") as f:
-    svg = f.read()
+
+file_list = [
+    "article.svg",
+    "article_light.svg"
+]
+
+for i in file_list:
+
+    # 读取 article.svg 文件
+    with open(os.path.join(i), "r", encoding="utf-8") as f:
+        svg = f.read()
 
 
-# 获取当前时间
-def get_now():
-    now = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
-    return now
+    # 获取当前时间
+    def get_now():
+        now = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+        return now
 
 
-for i in range(len(article_list)):
-    item = article_list[i]["title"].replace("&","")
-    if len(item) > 15:
-        item = item[:15] + ".."
-    svg = svg.replace("{title-%s}" % (i + 1), item)
-    svg = svg.replace("{time}", get_now())
+    for i in range(len(article_list)):
+        item = article_list[i]["title"].replace("&","")
+        if len(item) > 15:
+            item = item[:15] + ".."
+        svg = svg.replace("{title-%s}" % (i + 1), item)
+        svg = svg.replace("{time}", get_now())
 
 
-# 写入 \profile\article.svg
-with open(os.path.join("Profile", "article.svg"), "w", encoding="utf-8") as f:
-    f.write(svg)
+    # 写入 \profile\article.svg
+    with open(os.path.join("Profile", i), "w", encoding="utf-8") as f:
+        f.write(svg)
 
-print("完成")
+    print("完成")
